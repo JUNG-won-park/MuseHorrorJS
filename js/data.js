96ba3314b1d504b1b975f3c7c6f71f4bab60e83d
JUNG-won-park <ahg0525@gmail.com>
2022-03-29 17:07:15 +0900

    finished!

--- js/data.js
+++ js/data.js
@@ -3,45 +3,45 @@ const qnaList = [
     q: '1. Colorful life',
     img: './img/art1.png',
     a: [
-      { answer: 'a. Calvin Harris - Slide', type: ['0'] },
-      { answer: 'b. Doctor U - Many pills', type: ['5'] },
-      { answer: 'c. Demian Hirst - Utopia', type: ['10'] },
+      { answer: 'a. Calvin Harris - Slide', type: [2] },
+      { answer: 'b. Doctor U - Many pills', type: [1] },
+      { answer: 'c. Demian Hirst - Utopia', type: [0] },
     ]
   },
   {
     q: '2. They teach us nothing',
     img: './img/art2.png',
     a: [
-      { answer: 'a. Jake & Dinos Chapman - Minderwertigkinder', type: ['10'] },
-      { answer: 'b. Pablo Picasso - The animal kid', type: ['0'] },
-      { answer: 'c. Antoni Gaudí - Oftenly I build something but the animal art', type: ['5'] },
+      { answer: 'a. Jake & Dinos Chapman - Minderwertigkinder', type: [0] },
+      { answer: 'b. Pablo Picasso - The animal kid', type: [2] },
+      { answer: 'c. Antoni Gaudí - Oftenly I build something but the animal art', type: [1] },
     ]
   },
   {
     q: '3. Ill have BigMac today.',
     img: './img/art3.png',
     a: [
-      { answer: 'a. Jake & Dinos Chapman - Zombies, Coat Hangers, and Liquorice', type: ['10'] },
-      { answer: 'b. Ronald McDonald - We rule the world', type: ['0'] },
-      { answer: 'c. Donald Trump - Healthy meal for your lunch', type: ['5'] }
+      { answer: 'a. Jake & Dinos Chapman - Zombies, Coat Hangers, and Liquorice', type: [0] },
+      { answer: 'b. Ronald McDonald - We rule the world', type: [1] },
+      { answer: 'c. Donald Trump - Healthy meal for your lunch', type: [2] }
     ]
   },
   {
     q: '4. U R not scary anymore',
     img: './img/art4.png',
     a: [
-      { answer: 'a. Steven Spielberg - Jaws the Scary animal in the ocean', type: ['5'] },
-      { answer: 'b. Will Smith - Shark tale" ', type: ['0'] },
-      { answer: 'c. Demian Hirst - The Physical Impossibility of Death in the Mind of Someone Living ', type: ['10'] },
+      { answer: 'a. Steven Spielberg - Jaws the Scary animal in the ocean', type: [2] },
+      { answer: 'b. Will Smith - Shark tale" ', type: [1] },
+      { answer: 'c. Demian Hirst - The Physical Impossibility of Death in the Mind of Someone Living ', type: [0] },
     ]
   },
   {
     q: '5. 20200407',
     img: './img/art5.png',
     a: [
-      { answer: 'a. Pyramid', type: ['0'] },
-      { answer: 'b. Egyptian Museum', type: ['5']},
-      { answer: 'c. Muse Horror Concept art', type: ['10'] },
+      { answer: 'a. Pyramid', type: [1] },
+      { answer: 'b. Egyptian Museum', type: [2]},
+      { answer: 'c. Muse Horror Concept art', type: [0] },
     ]
   },
 
@@ -49,9 +49,9 @@ const qnaList = [
     q: '6. 2015',
     img: './img/art6.png',
     a: [
-      { answer: 'a. Cat', type: ['10'] },
-      { answer: 'b. Dog', type: ['5'] },
-      { answer: 'c. Tiger', type: ['0'] },
+      { answer: 'a. Cat', type: [0] },
+      { answer: 'b. Dog', type: [1] },
+      { answer: 'c. Tiger', type: [2] },
     ]
   }
 ]
@@ -59,7 +59,7 @@ const qnaList = [
 const infoList = [
   {
     name: 'Well done! Congratulation!',
-    desc: 'You are an expert. 🤟'
+    desc: 'GOOD 🤟'
   },
   {
     name: 'Good! Not bad.',
--- js/start.js
+++ js/start.js
@@ -1,40 +1,30 @@
 const main = document.querySelector('#main');
 const qna = document.querySelector('#qna');
 const result = document.querySelector('#result');
-const endPoint = qnaList.length;
-const select = [];
+const endPoint = qnaList.length; //6
+const select = [0,0,0,0,0,0];
 
 function calResult(){
-    let pointArray = [
-        {name: '10', value: 0, key: 0},
-        {name: '5', value: 0, key: 1},
-        {name: '0', value: 0, key: 2}
-    ]
-    for(let i = 0; i < endPoint; i++) {
-        let target = qnaList[i].a[select[i]];
-        for(let j=0; j < target.type.length; j++) {
-            for(let k=0; k< pointArray.length; k++) {
-                if(target.type[j] === pointArray[k].name){
-                    pointArray[k].value += 1;
-                }
-            }
-        }
-    }
+    let result = select.indexOf(Math.max(...select));
+    return result;
+}
 
-    let resultArray = pointArray.sort(function (a,b) {
-        if(a.value > b.value) {
-            return -1;
-        }
-        if(a.value < b.value) {
-            return 1;
-        }
-        return 0;
-    });
+function setResult(){
+    let point = calResult(); 
+    const resultName = document.querySelector('.resultName')
+    resultName.innerHTML = infoList[point].name;
 
-    console.log(resultArray);
+    let resultImg = document.createElement('img');
+    const imgDiv = document.querySelector('#resultImg');
+    var imgURL = `img/image-${point}.png`;
+    
+    resultImg.src = imgURL;
+    resultImg.alt = point;
+    resultImg.classList.add('img-fluid');
+    imgDiv.appendChild(resultImg);
 
-    let resultWord = resultArray[0].key;
-    return resultWord;
+    const resultDesc = document.querySelector('.resultDesc');
+    resultDesc.innerHTML = infoList[point].desc;
 }
 
 function goResult() {
@@ -47,8 +37,8 @@ function goResult() {
             qna.style.display = "none";
             result.style.display = "block";
         },450)})
-
-    calResult();
+        
+    setResult();
 }
 
 function addAnswer(answerText, qIdx, idx) {
@@ -70,8 +60,11 @@ function addAnswer(answerText, qIdx, idx) {
             children[i].style.animation = "fadeOut 0.5s";
         }
         setTimeout(()=>{
-            select[qIdx] = idx;
-
+            let target = qnaList[qIdx].a[idx].type;
+            for(let j=0; j < target.length; j++) {
+                select[target[j]] += 1;
+            }
+            
             for(let i=0; i < children.length; i++) {
                 children[i].style.display = 'none';
             }
